1d2b0c35dfa4551312f1da58fe53a29ffe359169
Nicolas <nicbuirod@hotmail.com>
2022-11-18 19:35:46 -0500

    ejercicio JS

---
+++ objeto.js
@@ -0,0 +1,31 @@
+function myFunction(a, b) {
+  var llaves = Object.keys(a);
+  let temp = 0;
+  for (var i = 0; i < llaves.length; i++) {
+    var llave = llaves[i];
+    var contenido = a[llave];
+    resultado = llave.search(b);
+
+
+    if (resultado != -1 && contenido!=undefined) {
+      temp = temp + 1;
+    } else {
+      temp = temp + 0;
+    }
+  }
+  
+  if (temp >= 1) {
+    
+    res=true
+    
+  } else {
+    res=false
+  }
+  
+  return res
+  
+}
+
+console.log(myFunction({ a: 1, b: 2, c: 3 }, "b"));
+console.log(myFunction({ x: "a", y: "b", z: "c" }, "a"));
+console.log(myFunction({ x: "a", y: "b", z: undefined }, "z"));
